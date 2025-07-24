4d2f7ee66f9def588d0a48ba4ecee5271aee9d90
Raja <chikotirajavignesh17@gmail.com>
2025-07-24 18:45:33 +0530

    uptimebot commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,9 @@ app.use(
     credentials: true,
   })
 );
-
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
